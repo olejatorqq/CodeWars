0956caba746a05475e3ba66d63abe8c4b25edf2a
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-09-21 17:34:56 +0300

    Exes and Ohs
    Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.

---
+++ Exes and Ohs/Exes and Ohs.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System;
+
+Console.WriteLine(Kata.XO("xXxXasdooOO"));
+
+public static class Kata 
+{
+    public static bool XO (string input)
+    {
+        //best solution
+        return input.ToLower().Count(i => i == 'x') == input.ToLower().Count(i => i == 'o');
+        
+        //my solution
+        //
+        // int countO = 0;
+        // int countX = 0;
+        // foreach (var helper in input.ToLower())
+        // {
+        //     if (helper.ToString() == "o")
+        //     {
+        //         countO++;
+        //     }else if (helper.ToString() == "x")
+        //     {
+        //         countX++;
+        //     }
+        // }
+        //
+        // if (countO == countX)
+        // {
+        //     return true;
+        // }
+        // return false;
+    }
+}
