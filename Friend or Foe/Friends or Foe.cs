a9dd1890fcb90ec379dad7403fce6ee3c0f6ce96
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-09-22 14:14:18 +0300

    Friend or Foe
    Make a program that filters a list of strings and returns a list with only your friends name in it.
    If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...

---
+++ Friend or Foe/Friends or Foe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+Console.WriteLine(Kata.FriendOrFoe(new []{"Oleg","Max","Champion","Magamed","Ivan"}));
+
+
+public static class Kata {
+    public static IEnumerable<string> FriendOrFoe (string[] names)
+    {
+        //best solution
+        return names.Where(name => name.Length == 4);
+
+        // my solution
+
+        // var letters4 = from name in names
+        //     where name.Length == 4
+        //     select name;
+        //
+        // return letters4;
+    }
+}
