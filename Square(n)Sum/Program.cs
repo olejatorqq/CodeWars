0c675fda0d77f6ff90575760a4bba6ab3502e749
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-09-22 15:27:55 +0300

    Square(n)Sum
    Complete the square sum function so that it squares each number passed into it and then sums the results together.

---
+++ Square(n)Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(Kata.SquareSum(new []{1,2,3}));
+
+public static class Kata
+{
+    public static int SquareSum(int[] numbers)
+    {
+        //best solution
+        return numbers.Sum(x => x * x);
+
+
+        //my solution
+
+        // double power = 0;
+        // for (int i = 0; i < numbers.Length; i++)
+        // {
+        //     power += Math.Pow(numbers[i], 2);
+        // }
+        //
+        // return (int)power;
+    }
+}
