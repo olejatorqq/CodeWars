37f455cde36ce0b7a120698e2bab7cc886970895
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-09-23 10:34:32 +0300

    Unique in Order
    Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.

---
+++ Unique in Order/Uniquie in Order.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+
+Console.WriteLine(Kata.UniqueInOrder(new[] {"ABBCcAD"}));
+
+public static class Kata
+{
+  public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+  {
+    //best solution
+    T previous = default(T);
+    foreach(T current in iterable)
+    {
+      if (!current.Equals(previous))
+      {
+        previous = current;
+        //yield????????????????
+        yield return current;
+      }
+    }
+
+
+    //this is the most difficult task for me (My solution) (it doesn't work)
+
+    // var answer = iterable.Distinct();
+    // return answer;
+  }
+}
