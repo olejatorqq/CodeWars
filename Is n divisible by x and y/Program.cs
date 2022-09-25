4f8ccf3f859b5ca0902c40200b5a20b2158d5d3d
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-09-25 12:32:55 +0300

    Is n divisible by x and y?
    Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.

---
+++ Is n divisible by x and y/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine(DivisibleNb.isDivisible(12,2,3));
+
+public class DivisibleNb 
+{
+    public static bool isDivisible(long n, long x, long y)
+    {
+        //my solution is the best
+        return n % x == 0 && n % y == 0;
+    }
+}
