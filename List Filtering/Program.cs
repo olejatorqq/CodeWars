718c6c7e6d55267828bdfa193042cb3a6feb0d66
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-09-26 17:35:31 +0300

    List Filtering
    In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.

---
+++ List Filtering/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+Console.Write(ListFilterer.GetIntegersFromList(new List<object>(){1,"a","b",0,15}));
+
+public class ListFilterer
+{
+    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+    {
+        //best solution
+        return listOfItems.OfType<int>();
+
+
+
+        //my solution
+
+        // List<int> returnList = new List<int>();
+        //
+        // foreach (var item in listOfItems)
+        // {
+        //     if (item is int)
+        //     {
+        //         returnList.Add((int)item);
+        //     }
+        // }
+        //
+        // return returnList;
+    }
+}
