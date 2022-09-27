ae96d150b2fdbf9dee106047e7da9522c54a68b6
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-09-27 13:38:34 +0300

    You are a square
    Given an integral number, determine if it's a square number

---
+++ You are a square/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.Write(Kata.IsSquare(90264));
+
+public class Kata
+{
+    public static bool IsSquare(int n)
+    {
+        //best solution
+        return Math.Sqrt(n) % 1 == 0;
+
+        //my solution
+
+        // int answer = (int)Math.Sqrt(n);
+        // return Math.Pow(answer,2) == n ? true : false;
+    }
+}
