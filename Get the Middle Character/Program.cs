c9d3881d831f9aa3964cec70eea878205ea97e9b
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-10-03 18:26:18 +0300

    Get the Middle Character
    You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.

---
+++ Get the Middle Character/Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.Write(Kata.GetMiddle("zxcqwerzxq"));
+
+public class Kata
+{
+    public static string GetMiddle(string s)
+    {
+        //best solution
+        if (s.Length % 2 == 0) {
+            return s.Substring(s.Length/2-1, 2);
+        } else {
+            return s.Substring(s.Length/2, 1);
+        }
+        
+        //my solution
+        
+        // if (s.Length < 3)
+        // {
+        //     return s;
+        // }
+        // else
+        // {
+        //     if (s.Length % 2 == 0)
+        //     {
+        //         return s[s.Length / 2 - 1] + s[s.Length / 2].ToString();
+        //     }
+        //
+        //     return s[s.Length / 2].ToString();
+        // }
+
+
+    }
+}
