e855076371e09b8f0a6d8784272d1b39de2f5ef4
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-10-04 19:11:09 +0300

    Count of positives / sum of negatives
    Given an array of integers.
    Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
    If the input is an empty array or is null, return an empty array.

---
+++ Count of positives  sum of negatives/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+Console.Write(Kata.CountPositivesSumNegatives(new int[] {0,0}));
+
+public class Kata
+{
+    public static int[] CountPositivesSumNegatives(int[] input)
+    {
+        //my solution was the best
+        Console.WriteLine(input.Max()+" "+ input.Where(i => i < 0).Sum());
+        return new int[] {input.Max(), input.Where(i => i < 0).Sum() };
+    }
+}
