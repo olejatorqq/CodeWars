aab61fb7b06c8bddc66f8d9ed286f43fd5c1fa55
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-10-05 19:50:52 +0300

    Insert values
    Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.

---
+++ Invert values/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+Console.WriteLine(ArraysInversion.InvertValues(new int[] {-1,2,-3,-4,-5}));
+
+public static class ArraysInversion
+{
+    public static int[] InvertValues(int[] input)
+    {
+        //best solution
+        return input.Select(n => -n).ToArray();
+
+        //my solution
+
+        // var qq = from i in input
+        //     select i*-1;
+        //
+        // return qq.ToArray();
+    }
+}
