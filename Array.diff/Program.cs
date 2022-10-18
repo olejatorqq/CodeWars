04876213279667a789612fcd1b7957381b7cd193
Oleg Orlovsky <oleg.orlowsky@gmail.com>
2022-10-18 18:35:20 +0300

    Array.diff
    Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
    It should remove all values from list a, which are present in list b keeping their order.

---
+++ Array.diff/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] firstArray = new[] { 1, 2, 2, 2, 3 };
+int[] secondArray = new[] { 2, 3 };
+
+
+Console.WriteLine(Kata.ArrayDiff(firstArray,secondArray));
+
+public class Kata
+{
+    public static int[] ArrayDiff(int[] a, int[] b)
+    {
+        //best solution
+        return a.Where(i => !b.Contains(i)).ToArray();
+
+        //my solution (so close :))
+        // var qq = from i in a
+        //     where !b.Contains(i)
+        //     select i;
+        //
+        // return qq.ToArray();
+    }
+}
